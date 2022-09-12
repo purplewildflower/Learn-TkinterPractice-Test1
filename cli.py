64a65d2adf2942632ea4bda436256738c41f67e8
Emily Harrison <ehemilyharrison@gmail.com>
2022-09-12 14:18:26 -0600

    Added cli.py & main.py

---
+++ cli.py
@@ -0,0 +1,4 @@
+from subdir.main import *
+
+if __name__ == '__main__':
+    main = MainGUI()
---
+++ subdir/main.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class MainGUI:
+    def __init__(self):
+        print(f"Hello")
